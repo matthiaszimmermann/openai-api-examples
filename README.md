c6cf56b3a2fe6eac2940b08deae147895f013b37
Matthias Zimmermann <zimmermann@xbrain.ch>
2023-06-11 14:25:05 +0000

    add 'hello world' example and readme

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# openai-api-examples
+# OpenAI API Examples
+
+## IDE Preparations
+
+This repository is built for usage with VS Code and devcontainers.
+
+The devcontainer setup takes care of the necessary installations to work with Python, the OpenAI library and other necessary components.
+
+## API Usage Preparations
+
+* You need to be signed up on [OpenAI](https://platform.openai.com/).
+* In your account settings you need to provide your payment method (eg. credit card details)
+* In your account settings you need to create an API KEY (and store that key in a save place)
+
+## "Hello World"
+
+The goal of this section is to verify that your end-to-end setup is working properly.
+
+The content is inspired by [OpenAI's question answering tutorial](https://github.com/openai/openai-cookbook/blob/main/examples/Question_answering_using_embeddings.ipynb).
+
+The "Hello World" task is to answer questions about an article provided to the program.
+As a default a [Wipedia article](https://en.wikipedia.org/wiki/Curling_at_the_2022_Winter_Olympics) about curling in the 2022 Olympics is used.
+
+Run the program as shown below
+
+```bash
+python hello_world.py
+```
+
+Example questions:
+
+* Which athletes won the gold medal in curling at the 2022 Winter Olympics?
+* List the countries that participated in the 2022 curling winter olypmic games
+* List the team members of all teams winning gold in the 2022 curling olympics
---
+++ hello_world.py
@@ -0,0 +1,73 @@
+# import ast  # for converting embeddings saved as strings back to arrays
+import openai  # for calling the OpenAI API
+# import pandas as pd  # for storing text and embeddings data
+# import tiktoken  # for counting tokens
+# from scipy import spatial  # for calculating vector similarities for search
+import sys
+
+FILE_NAME = './data/wikipedia_curling.txt'
+QUIT_WORD = 'quit'
+
+GPT_35 = "gpt-3.5-turbo"
+GPT_40 = "gpt-4"
+GPT_MODEL = GPT_35
+
+QUERY_PREFIX = """
+Use the below article to answer the subsequent question marked "Question:". 
+If the answer cannot be found, write "I don't know."
+
+Article:
+"""
+
+def read_file(filename:str) -> str:
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+
+    return '\n'.join(lines)
+
+
+def create_query(article:str, user_question:str) -> str:
+    return QUERY_PREFIX + article + "\nQuestion: " + user_question
+
+
+def get_answer(article:str, user_question:str) -> str:
+    user_query = create_query(article, user_question)
+
+    response = openai.ChatCompletion.create(
+        messages = [
+            {'role': 'system', 'content': 'You answer questions about the provided article.'},
+            {'role': 'user', 'content': user_query },
+        ],
+        model = GPT_MODEL,
+        temperature = 0,
+    )
+
+    return response['choices'][0]['message']['content']
+
+
+def main():
+    file_name = FILE_NAME
+
+    # check command line
+    if len(sys.argv) > 2:
+        print("usage: {} [article-file]".format(sys.argv[0]))
+        sys.exit(1)
+    elif len(sys.argv) == 2:
+        file_name = sys.argv[1]
+
+    # load text of article
+    article = read_file(file_name)
+
+    # answer user questions
+    while True:
+        user_question = input('Ask a question about article {} (or "{}" to exit): '.format(file_name, QUIT_WORD))
+        if user_question.lower() == QUIT_WORD:
+            break
+
+        answer = get_answer(article, user_question)
+
+        print(answer)
+
+
+if __name__ == '__main__':
+    main()
