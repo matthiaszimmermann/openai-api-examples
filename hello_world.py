9073316ffd1ad53dfb13186cf20db4b0c7190dd7
Matthias Zimmermann <zimmermann@xbrain.ch>
2023-06-11 16:37:24 +0000

    cleanup imports for hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,14 +1,11 @@
-# import ast  # for converting embeddings saved as strings back to arrays
 import openai  # for calling the OpenAI API
-# import pandas as pd  # for storing text and embeddings data
-# import tiktoken  # for counting tokens
-# from scipy import spatial  # for calculating vector similarities for search
 import sys
 
 FILE_NAME = './data/wikipedia_curling.txt'
 QUIT_WORD = 'quit'
 
 GPT_35 = "gpt-3.5-turbo"
+# apply here for access to gpt-4 https://openai.com/waitlist/gpt-4-api
 GPT_40 = "gpt-4"
 GPT_MODEL = GPT_35
 
